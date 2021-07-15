e8cd11ae82ba3919a600fac5d36d1b81a6139528
Pattaranon Plyduang <pattaranon.ply@gmail.com>
2021-07-15 15:40:29 +0700

    Build CAB file

--- Calbee.Infra.Common/Constants/IConstants.cs
+++ Calbee.Infra.Common/Constants/IConstants.cs
@@ -52,6 +52,6 @@ namespace Calbee.Infra.Common.Constants
         /// Y = Windows CE
         /// N = Windows Client
         /// </summary>
-        public static string platformWinCE = "N";
+        public static string platformWinCE = "Y";
     }
 }
